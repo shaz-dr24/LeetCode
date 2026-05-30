932f13c169c7ccc4ddef1273ab6123047eb1f64b
Shachindran H A <shachindranrao@gmail.com>
2026-05-30 16:48:34 +0530

    corrected code

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,25 +1,25 @@
 class Solution {
-    public int countPrimes(int n) {
 
-        boolean[] prime = new boolean[n];
-
-        for(int i=2;i<n;i++){
-            prime[i] = true;
+    public static boolean isPrime(int n){
+        if(n <= 1){
+            return false;
         }
 
-        for(int i=2;i*i<n;i++){
-            if(prime[i]){
-                for(int j=i*i;j<n;j=j+i){
-                    prime[j] = false;
-                }
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0){
+                return false;
             }
         }
 
+        return true;
+    }
+
+    public int countPrimes(int n) {
         int count = 0;
 
-        for(int i=2;i<n;i++){
-            if(prime[i]){
-                count++;
+        for(int i = 0; i < n; i++){
+            if(isPrime(i)){
+                count = count + 1;
             }
         }
 
