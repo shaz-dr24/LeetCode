7782f45362c98814204e5a56dbcfd3875db924f8
Shachindran H A <shachindranrao@gmail.com>
2026-06-23 20:12:30 +0530

    Time: 155 ms (5.85%) | Memory: 47.1 MB (5.07%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,39 +1,15 @@
-import java.util.*;
-
 class Solution {
     public boolean isValid(String s) {
 
-        Stack<Character> stack = new Stack<>();
+        while (s.contains("()") ||
+               s.contains("{}") ||
+               s.contains("[]")) {
 
-        for(int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-
-            if(ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            }
-            else {
-
-                if(stack.isEmpty()) {
-                    return false;
-                }
-
-                char top = stack.pop();
-
-                if(ch == ')' && top != '(') {
-                    return false;
-                }
-
-                if(ch == '}' && top != '{') {
-                    return false;
-                }
-
-                if(ch == ']' && top != '[') {
-                    return false;
-                }
-            }
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
         }
 
-        return stack.isEmpty();
+        return s.length() == 0;
     }
 }
