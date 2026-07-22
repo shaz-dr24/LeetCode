5ea1302e6fb138e541ba798c03b0ea8e990e0de4
Shachindran H A <shachindranrao@gmail.com>
2026-07-22 10:54:20 +0530

    Time: 5 ms (10.07%) | Memory: 44 MB (13.92%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,10 +1,9 @@
 import java.math.*;
-
 class Solution {
     public String addBinary(String a, String b) {
-        BigInteger b1 = new BigInteger(a, 2);
-        BigInteger b2 = new BigInteger(b, 2);
-        BigInteger b3 = b1.add(b2);
+        BigInteger b1=new BigInteger(a,2);
+        BigInteger b2=new BigInteger(b,2);
+        BigInteger b3=b1.add(b2);
         return b3.toString(2);
     }
 }
