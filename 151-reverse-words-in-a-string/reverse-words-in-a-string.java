61045098dc344fa59e626f4f620d33526c229c8f
Shachindran H A <shachindranrao@gmail.com>
2026-07-23 20:31:57 +0530

    Time: 6 ms (80.59%) | Memory: 45.7 MB (20.13%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,12 +1,20 @@
+import java.util.*;
 class Solution {
     public String reverseWords(String s) {
-        String[] strarr = s.split(" ");
-        StringBuffer sb = new StringBuffer("");
-        for (int i = strarr.length - 1; i >= 0; i--) {
-            if (!strarr[i].equals("")) {
-                sb.append(strarr[i]).append(" ");
-            }
+        s=s.trim();
+        s = s.replaceAll(" +", " ");
+        String [] arr=s.split(" ");
+        String [] ans=new String[arr.length];
+        int j=0;
+        for(int i=arr.length-1;i>=0;i--) {
+            ans[j]=arr[i];
+            j=j+1;
         }
-        return sb.toString().trim();
+        StringBuffer sb=new StringBuffer ("");
+        for(int i=0;i<ans.length;i++) {
+            sb.append(ans[i]+" ");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+       return sb.toString();
     }
 }
