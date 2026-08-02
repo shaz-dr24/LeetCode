35b6e15336d930f2d56ba2d601a8dba2a27e39c6
Shachindran H A <shachindranrao@gmail.com>
2026-08-02 13:05:30 +0530

    Time: 15 ms (59.85%) | Memory: 51.6 MB (37.38%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,11 +1,11 @@
 import java.util.*;
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-     
+        int i=0;
+        int j=0;
         Arrays.sort(g);
         Arrays.sort(s);
-           int i=0;
-        int j=0;
+        
         while(i<g.length&&j<s.length){
             if(s[j]>=g[i]){
                 i=i+1;
