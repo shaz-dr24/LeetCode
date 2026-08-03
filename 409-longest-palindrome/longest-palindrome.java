aa74fc80232981725b25b6a07be9786291e5ba5c
Shachindran H A <shachindranrao@gmail.com>
2026-08-03 19:54:48 +0530

    Time: 10 ms (7.33%) | Memory: 43.1 MB (57.87%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -5,23 +5,29 @@ class Solution {
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(map.containsKey(ch)){
-                map.put(ch, map.get(ch)+1);
+                map.put(ch,map.get(ch)+1);
             }
             else{
                 map.put(ch,1);
             }
         }
-        int ans=0;
-        for(Integer x: map.values()){
-            if(x%2==0){
-                ans=ans+x;
+
+        int evesum=0;
+        int oddsum=0;
+        boolean oddPresent=false;
+        for(Map.Entry<Character, Integer> entry: map.entrySet()){
+            if(entry.getValue()%2==0){
+                evesum=evesum+entry.getValue();
+
             }
             else{
-                ans=ans+(x-1);
+                oddsum=oddsum+(entry.getValue()-1);
+                oddPresent=true;
             }
         }
-        if(ans<s.length()){
-          ans=ans+1;
+        int ans=evesum+oddsum;
+        if(oddPresent){
+            return ans+1;
         }
         return ans;
     }
