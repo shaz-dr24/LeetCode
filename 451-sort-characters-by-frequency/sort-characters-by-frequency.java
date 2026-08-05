16fccf2b5110e823fa98eee66b1a707b55be3626
Shachindran H A <shachindranrao@gmail.com>
2026-08-05 20:02:09 +0530

    Time: 21 ms (12.83%) | Memory: 46.7 MB (52.39%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,43 +1,37 @@
 import java.util.*;
-
 class Solution {
     public String frequencySort(String s) {
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        // Count frequency
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if (map.containsKey(ch)) {
-                map.put(ch, map.get(ch) + 1);
-            } else {
-                map.put(ch, 1);
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        while (!map.isEmpty()) {
-
-            int max = Collections.max(map.values());
-
-            char maxChar = ' ';
-
-            for (char ch : map.keySet()) {
-                if (map.get(ch) == max) {
-                    maxChar = ch;
-                    break;
-                }
-            }
-
-            for (int i = 0; i < max; i++) {
-                sb.append(maxChar);
-            }
-
-            map.remove(maxChar);
-        }
-
-        return sb.toString();
+        	HashMap<Character, Integer> map=new HashMap<>();
+	for(int i=0;i<s.length();i++) {
+		char ch=s.charAt(i);
+		if(map.containsKey(ch)) {
+			map.put(ch, map.get(ch)+1);
+		}
+		else {
+			map.put(ch,1);
+		}
+	}
+	
+	ArrayList<Map.Entry<Character, Integer>> al=new ArrayList<>(map.entrySet());
+	Collections.sort(al, (a,b)->{
+		if(b.getValue()!=a.getValue()) {
+			return b.getValue()-a.getValue();
+		}
+		else {
+			return a.getKey().compareTo(b.getKey());
+		}
+	});
+	
+	LinkedHashMap<Character, Integer> sortedmap=new LinkedHashMap<>();
+	for(Map.Entry<Character, Integer>entry: al) {
+		sortedmap.put(entry.getKey(),entry.getValue());
+	}
+	//System.out.println(sortedmap);
+	
+	StringBuffer sb=new StringBuffer("");
+	for(Map.Entry<Character, Integer> entry: sortedmap.entrySet()) {
+			sb.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+	}
+    return sb.toString();
     }
 }
