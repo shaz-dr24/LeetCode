8f1b2f32fe8527bacb6c168c987a8575e158d0b1
Shachindran H A <shachindranrao@gmail.com>
2026-08-07 14:22:28 +0530

    Time: 22 ms (6.32%) | Memory: 80.1 MB (14.60%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,31 +1,30 @@
 import java.util.*;
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        ArrayList<Integer> al = new ArrayList<>();
-        // Store only positive numbers
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                al.add(nums[i]);
-            }
-        }
-        int[] arr = new int[al.size()];
-        for (int i = 0; i < al.size(); i++) {
-            arr[i] = al.get(i);
-        }
-        Arrays.sort(arr);
-        int expected = 1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == expected) {
-                expected++;
-            }
-            // Ignore duplicates
-            else if (arr[i] < expected) {
-                continue;
-            }
-            else {
-                break;
-            }
-        }
-        return expected;
+    public int firstMissingPositive(int[] arr) {
+ArrayList<Integer> al=new ArrayList<>();
+for(int i=0;i<arr.length;i++){
+    if(arr[i]>0) {
+    	al.add(arr[i]);
+    }
+}
+int [] res=new int[al.size()];
+for(int i=0;i<al.size();i++) {
+	res[i]=al.get(i);
+}
+Arrays.sort(res);
+int expected=1;
+for(int i=0;i<res.length;i++) {
+	if(res[i]==expected) {
+		expected=expected+1;
+	}
+	else if(res[i]<expected) {
+		continue;
+	}
+	else {
+		break;
+	}
+}
+//System.out.println(expected);
+return expected;
     }
 }
