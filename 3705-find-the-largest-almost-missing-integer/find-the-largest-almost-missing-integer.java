9f8698052c5d78c8c508f4acd0a3adaa64246e84
Shachindran H A <shachindranrao@gmail.com>
2026-08-18 20:19:05 +0530

    Time: 6 ms (37.19%) | Memory: 45.3 MB (48.07%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,38 +1,32 @@
+import java.util.*;
 class Solution {
-    public int largestInteger(int[] nums, int k) {
-
-        int[] temp = nums.clone();
+    public int largestInteger(int[] arr, int k) {
+        int []temp=arr.clone();
         Arrays.sort(temp);
-
-        int ans = -1;
-
-        for (int a = 0; a < temp.length; a++) {
-
-            int candidate = temp[a];
-            int count = 0;
-
-            for (int i = 0; i <= nums.length - k; i++) {
-
-                boolean found = false;
-
-                for (int j = i; j < i + k; j++) {
-
-                    if (nums[j] == candidate) {
-                        found = true;
+        int ans=Integer.MIN_VALUE;
+        for(int x=0;x<temp.length;x++){
+            int val=temp[x];
+            int count=0;
+            for(int i=0;i<=arr.length-k;i++){
+                boolean flag=false;
+                for(int j=i;j<i+k;j++){
+                    if(arr[j]==val){
+                        //count=count+1;
+                        flag=true;
                         break;
                     }
                 }
-
-                if (found) {
-                    count++;
+                if(flag){
+                     count=count+1;
                 }
             }
-
-            if (count == 1) {
-                ans = Math.max(ans, candidate);
+            if(count==1){
+                ans=Math.max(val, ans);
             }
         }
-
+        if(ans==Integer.MIN_VALUE){
+            return -1;
+        }
         return ans;
     }
 }
