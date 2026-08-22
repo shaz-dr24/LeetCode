a409a3f88734d86ce746d4e003dea6a07b25603a
Shachindran H A <shachindranrao@gmail.com>
2026-08-22 14:56:40 +0530

    Time: 22 ms (6.96%) | Memory: 50.9 MB (5.01%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,33 +1,47 @@
 import java.util.*;
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-            } else {
-                map.put(nums[i], 1);
-            }
-        }
-        PriorityQueue<Integer> pq = new PriorityQueue<>
-        (new Comparator<Integer>() {
-                @Override
-                public int compare(Integer a, Integer b) {
-                    return map.get(b) - map.get(a);
-                }
-            }
-        );
-        for (Integer x : map.keySet()) {
-            pq.offer(x);
-        }
-        ArrayList<Integer> al = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            al.add(pq.poll());
-        }
-        int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
-            ans[i] = al.get(i);
-        }
-        return ans;
+    public int[] topKFrequent(int[] arr, int k) {
+        	HashMap<Integer,Integer> map=new HashMap<>();
+	for(int i=0;i<arr.length;i++) {
+		if(map.containsKey(arr[i])) {
+			map.put(arr[i], map.get(arr[i])+1);
+		}
+		else {
+			map.put(arr[i],1);
+		}
+	}
+	//System.out.println(map);
+	ArrayList<Map.Entry<Integer, Integer>>al=new ArrayList<>(map.entrySet());
+	Collections.sort(al,(a,b)->{
+		if(a.getValue()!=b.getValue()) {
+			return b.getValue()-a.getValue();
+		}
+		else {
+			return b.getKey().compareTo(a.getKey());
+		}
+	});
+	LinkedHashMap<Integer, Integer> sortedmap=new LinkedHashMap<>();
+	for(Map.Entry<Integer, Integer> entry: al) {
+		sortedmap.put(entry.getKey(), entry.getValue());
+	}
+	System.out.println(sortedmap);
+	ArrayList<Integer> res=new ArrayList<>();
+	for(Map.Entry<Integer,Integer> entry:sortedmap.entrySet()) {
+		if(k>0) {
+			res.add(entry.getKey());
+			k--;
+		}
+		else {
+			break;
+		}
+	}
+	Collections.reverse(res);
+	//System.out.println(res);
+	int [] ans=new int[res.size()];
+	for(int i=0;i<res.size();i++) {
+		ans[i]=res.get(i);
+	}
+	//System.out.println(Arrays.toString(ans));
+    return ans;
     }
 }
